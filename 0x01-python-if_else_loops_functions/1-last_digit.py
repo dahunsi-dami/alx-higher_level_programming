340b9ad46e5f11ab785af4a8a78c7730cb64ec6b
Dre <gbemigadare@gmail.com>
2024-01-03 03:32:41 +0100

    Complete code from line 4 to print specified strings.

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    n = number * -1
+    n = n % 10
+else:
+    n = number % 10
+print("Last digit of {}".format(number), end = ' ')
+if n > 5:
+    print("is {} and is greater than 5".format(n))
+elif n == 0:
+    print("is {} and is 0".format(n))
+elif n < 6 and n != 0:
+    print("is {} and is less than 6 and not 0".format(n))
