05d2c133ee550ecb8653206eff8eed282720d186
Dre <gbemigadare@gmail.com>
2024-01-08 17:47:18 +0100

    Don't use obj.reverse on original list as it alters original lists.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in my_list:
+    cop = my_list.copy()
+    cop.reverse()
+    for i in cop:
         print("{:d}".format(i))
