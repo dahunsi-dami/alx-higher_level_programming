66bfba64b330e57accd7b6480ee832ed61d7bafb
Dre <gbemigadare@gmail.com>
2024-01-09 10:12:45 +0100

    Implement edge case for if list is not None.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    llen = len(my_list) - 1
-    while (llen >= 0):
-        print("{:d}".format(my_list[llen]))
-        llen = llen - 1
+    if my_list is not None:
+        llen = len(my_list) - 1
+        while (llen >= 0):
+            print("{:d}".format(my_list[llen]))
+            llen = llen - 1
