1bacfe1b17a0d50225884418b0eba08009bda73e
Dre <gbemigadare@gmail.com>
2024-01-09 15:44:11 +0100

    Adjust base case to work where list length > 7 & empty str to empty list.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == "":
+    if my_list == []:
         return None
     llen = len(my_list)
     i = 0
     while i < llen:
-        if i == 7:
+        if i == (llen - 1):
             if my_list[i] < my_list[i - 1]:
                 temp = my_list[i]
                 my_list[i] = my_list[i - 1]
@@ -19,4 +19,5 @@ def max_integer(my_list=[]):
                 my_list[i + 1] = temp
         i = i + 1
     hi = my_list[-1]
+    print(my_list)
     return hi
