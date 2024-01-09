73ebfb21837ee0d95d6ab82fdca51f3db735b220
Dre <gbemigadare@gmail.com>
2024-01-09 10:58:40 +0100

    Use original list in solution.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = my_string.translate({99: 0})
-    sresult = result.translate({67: 0})
-    return sresult
+    my_string = my_string.translate({99: 0})
+    my_string = my_string.translate({67: 0})
+    return my_string
