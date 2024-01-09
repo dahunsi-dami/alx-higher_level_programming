806659c70e377266b47f3114bcddf16edeaaf07f
Dre <gbemigadare@gmail.com>
2024-01-09 13:37:26 +0100

    Add code for edge case where sentence is None.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    slen = len(sentence)
-    nt = ()
-    nt = nt + (slen, )
-    fchar = sentence[0]
-    nt = nt + (fchar, )
-    return nt
+    if sentence is not None:
+        slen = len(sentence)
+        nt = ()
+        nt = nt + (slen, )
+        fchar = sentence[0]
+        nt = nt + (fchar, )
+        return nt
