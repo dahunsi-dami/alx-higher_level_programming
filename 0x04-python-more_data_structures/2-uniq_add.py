6044f7d203ca3c2d3cccdcdf55152f5e89f3a1de
Dre <gbemigadare@gmail.com>
2024-01-12 15:52:48 +0100

    Convert list to set to get unique elements.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    for i in my_list:
-        if my_list.count(i) == 1:
-            sum += i
+    my_s = set(my_list)
+    for i in my_s:
+        sum += i
+    print(my_list)
+    print(my_s)
     return sum
