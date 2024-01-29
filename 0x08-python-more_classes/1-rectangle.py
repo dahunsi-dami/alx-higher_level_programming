42d7aeffc761d5e4e7637eb62fe94859fe547179
Dre <gbemigadare@gmail.com>
2024-01-29 14:20:45 +0100

    Add docstrings to code.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
+"""
+Rectangle class definition.
+"""
+
+
 class Rectangle:
+    """ Object representation of a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Initialization of rectangle's attributes."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """getter alternative for private instance attribute width."""
         return self.__width
 
     @property
     def height(self):
+        """getter alternative for private instance attribute height."""
         return self.__height
 
     @width.setter
     def width(self, value):
+        """setter alternative for private instance attribute width."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,6 +34,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """setter alternative for private instance attribute height."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
