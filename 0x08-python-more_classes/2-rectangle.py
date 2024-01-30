2d869ed545b9279a4583a8b7e795197eff1e8c3c
Dre <gbemigadare@gmail.com>
2024-01-30 03:50:18 +0100

    Add docstrings to code.

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
+"""Rectangle class definition."""
+
+
 class Rectangle:
+    """ Object representation of a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Initialization of rectangle's attributes."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """getter alternative for private instance attribute width."""
         return self.__width
 
     @property
     def height(self):
+        """getter alternative for private instance attribute height."""
         return self.__height
 
     @width.setter
     def width(self, value):
+        """setter alternative for private instance attribute width."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,6 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """setter alternative for private instance attribute height."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
@@ -32,9 +41,11 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """calculates and returns area of a rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
+        """calculates and returns perimeter of a rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return ((self.__width + self.__height) * 2)
