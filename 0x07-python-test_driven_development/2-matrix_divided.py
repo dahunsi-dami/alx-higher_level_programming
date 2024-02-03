b80fd79c609fca9906dc6a34eb9a792a5e4cc1e3
Dre <gbemigadare@gmail.com>
2024-02-03 04:53:02 +0100

    Reshuffle control flow in 2-matrix_divided.py.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,21 +7,22 @@ Module to divide all elements of a matrix
 def matrix_divided(matrix, div):
     """Returns new matrix with all elements in old matrix divided."""
 
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(msg)
 
-    rlen = len(matrix[0])
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    lent = len(matrix[0])
     for row in matrix:
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(msg)
 
-        if len(row) != rlen:
+        if lent != len(row):
             raise TypeError("Each row of the matrix must have the same size")
-
-        if type(div) is not int and type(div) is not float:
-            raise TypeError("div must be a number")
-
-        if div == 0:
-            raise ZeroDivisionError("division by zero")
 
     return [[round(item / div, 2) for item in row] for row in matrix]
