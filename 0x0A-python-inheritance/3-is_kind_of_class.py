f63e66d5bb099f2e16defafbd347f874030c95cc
Dre <gbemigadare@gmail.com>
2024-02-06 00:10:49 +0100

    Fix indentation & tabs @ line 7 for pycodestyle.

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,10 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """returns True if obj is instance of a_class
-		or instance of instance of a_class."""
+    """
+    returns True if obj is instance of a_class
+    or instance of instance of a_class.
+    """
 
     if isinstance(obj, a_class):
         return True
