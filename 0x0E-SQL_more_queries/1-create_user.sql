5e9e20c2f4317307ddc54411cfe784e0c1126362
dahunsi-dami <gbemigadare@gmail.com>
2024-03-13 19:50:57 +0100

    create user if they don't exist with given password.

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates MySQL server user `user_0d_1` w/ pswd, `user_0d_1_pwd`
+-- if user_0d_1 already exists, the script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
