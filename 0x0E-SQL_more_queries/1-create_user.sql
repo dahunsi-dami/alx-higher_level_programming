c6f5d4141480ad7d3c35781d7396de994e895211
dahunsi-dami <gbemigadare@gmail.com>
2024-03-13 20:25:32 +0100

    Add ; to statement and grant all privileges as specced.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- creates MySQL server user `user_0d_1` w/ pswd, `user_0d_1_pwd`
 -- if user_0d_1 already exists, the script should not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_od_1_pwd';
+FLUSH PRIVILEGES;
