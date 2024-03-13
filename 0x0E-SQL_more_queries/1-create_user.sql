459d3a58d12539aa05c2bce551ddc39ecd1dfc07
dahunsi-dami <gbemigadare@gmail.com>
2024-03-13 20:39:29 +0100

    Comment out grant-table-reloading command to see if all green.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- if user_0d_1 already exists, the script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- FLUSH PRIVILEGES;
