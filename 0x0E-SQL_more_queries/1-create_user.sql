144d4d72c4bc3dda85c28624b0ffcb67cb9214d0
dahunsi-dami <gbemigadare@gmail.com>
2024-03-13 20:42:29 +0100

    Remove password state on GRANT to see if all green.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- creates MySQL server user `user_0d_1` w/ pswd, `user_0d_1_pwd`
 -- if user_0d_1 already exists, the script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
