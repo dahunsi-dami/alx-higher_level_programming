1507492390d366cdb170d118d4b2207bbb413269
dahunsi-dami <gbemigadare@gmail.com>
2024-03-15 21:23:40 +0100

    List shows in db without a genre linked & other specs.

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,11 @@
+-- import database dump from s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- list all shows in hbtn_0d_tvshows that are without a genre linked
+-- display tv_shows.title, tv_show_genres.genre_id
+-- sort results in ascend order by tv_shows.title and tv_show_genres.genre.id
+-- you can only use SELECT statement
+-- db name will be passed as arg of mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
