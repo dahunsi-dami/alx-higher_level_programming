382f641f4414f704640e59ffbd450e5bd8565f8f
dahunsi-dami <gbemigadare@gmail.com>
2024-04-08 21:59:00 +0100

    Write script to print addition of 2 ints.

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+const firstArg = parseInt(args[2]);
+const secondArg = parseInt(args[3]);
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(firstArg, secondArg);
