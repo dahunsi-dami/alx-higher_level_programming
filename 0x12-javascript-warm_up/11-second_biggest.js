0b634de76b898c633d45f31063f70549150bad3b
dahunsi-dami <gbemigadare@gmail.com>
2024-04-09 01:27:06 +0100

    Adjust for test case list: 2, 2 && 2, 0.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-const coll = [];
+const coll = [0];
 let consid;
 let stud = -Infinity;
 let i = 2;
@@ -11,10 +11,7 @@ if (argv.length === 2 || argv.length === 3) {
 } else {
   while (i < argv.length) {
     consid = parseInt(argv[i]);
-    if (consid > coll[0] || coll.length === 0) {
-      if (coll.length === 0) {
-        coll[0] = consid;
-      }
+    if (consid >= coll[0] || coll.length === 0) {
       stud = coll[0];
       coll[0] = consid;
     } else if (consid > stud && consid !== coll[0]) {
