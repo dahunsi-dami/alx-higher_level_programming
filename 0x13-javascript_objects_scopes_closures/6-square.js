c6d2da3464f47c5cf228a116fdb2adf85168de46
dahunsi-dami <gbemigadare@gmail.com>
2024-04-10 02:01:46 +0100

    Let print char be X first else c, not 'C'.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,13 @@ const firstSquare = require('./5-square');
 
 class Square extends firstSquare {
   charPrint (c) {
-    let bluck = 'C';
+    let bluck;
 
     if (typeof c === 'undefined') {
       bluck = 'X';
-    }
+    } else {
+      bluck = c;
+	}
 
     for (let i = 0; i < this.height; i++) {
       let row = '';
