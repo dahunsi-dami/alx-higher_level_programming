dcfa8ae5cfbcd1c651191c76737ac4d2fe9d9b60
dahunsi-dami <gbemigadare@gmail.com>
2024-04-15 16:01:05 +0100

    Make pattern match case sensitive w/ BINARY.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             charset="utf8")
     cur = conn.cursor()
     cur.execute("""SELECT * FROM states
-                WHERE states.name LIKE 'N%'
+                WHERE BINARY states.name LIKE 'N%'
                 ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
