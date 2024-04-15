d527a70e696f08d70a96f907f572a09afdb05b21
dahunsi-dami <gbemigadare@gmail.com>
2024-04-16 00:30:24 +0100

    List all state objs containing 'a' from db.

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""list all state objects containing letter 'a' from hbtn_0e_6_usa db."""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    dlink = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+
+    engine = create_engine(dlink)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    search_term = 'a'
+    state_objs = session.query(State).filter(State.name.contains('a')).all()
+
+    for state in state_objs:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
