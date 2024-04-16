7bca6a6dd69a9377b71a356b7217d346ee5fcd12
dahunsi-dami <gbemigadare@gmail.com>
2024-04-17 00:34:58 +0100

    Add state obj 'Louisiana' to hbtn_0e_6_usa database.

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""add State obj `Louisiana` to hbtn_0e_6_usa db."""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    dlink = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+
+    engine = create_engine(dlink)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state_add = State(id=6, name='Louisiana')
+    session.add(state_add)
+
+    state = session.commit()
+
+    state_id = state_add.id
+    print(f"{state_id}")
+
+    session.close()
