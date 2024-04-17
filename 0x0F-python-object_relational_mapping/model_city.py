08946ccf8be3f81edc62a43b67e28d9af9c845fd
dahunsi-dami <gbemigadare@gmail.com>
2024-04-17 02:04:32 +0100

    Import State class in model_city.

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from model_state import Base
+from model_state import Base, State
 from sqlalchemy.orm import registry
 
 Base = declarative_base()
