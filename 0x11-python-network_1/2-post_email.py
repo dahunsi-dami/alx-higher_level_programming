86c383df702555a97f72463d9d3e42b0ac724e13
dahunsi-dami <gbemigadare@gmail.com>
2024-04-26 14:12:06 +0100

    Send POST request to URL w/ email as parameter.

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Send POST request to URL passed w/ email as parameter
+and show response body decoded in utf-8.
+"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+param = {"email": sys.argv[2]}
+data = urllib.parse.urlencode(param).encode('ascii')
+full_url = urllib.request.Request(sys.argv[1], data)
+
+with urllib.request.urlopen(full_url) as response:
+    risivd = response.read().decode("utf-8")
+    print(risivd)
