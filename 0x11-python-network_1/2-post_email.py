9fc26de1a1aeceb9c1bd517b14907f80e5dae08a
dahunsi-dami <gbemigadare@gmail.com>
2024-04-27 01:10:02 +0100

    Make code not run if imported.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,11 @@ import urllib.parse
 import urllib.request
 import sys
 
-param = {"email": sys.argv[2]}
-data = urllib.parse.urlencode(param).encode('ascii')
-full_url = urllib.request.Request(sys.argv[1], data)
+if __name__ == "__main__":
+    param = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(param).encode('ascii')
+    full_url = urllib.request.Request(sys.argv[1], data)
 
-with urllib.request.urlopen(full_url) as response:
-    risivd = response.read().decode("utf-8")
-    print(risivd)
+    with urllib.request.urlopen(full_url) as response:
+        risivd = response.read().decode("utf-8")
+        print(risivd)
