2a8bc9af86c3db1ff0a120a708a04cf4fb6d4ebd
dahunsi-dami <gbemigadare@gmail.com>
2024-04-30 17:52:58 +0100

    Raise HTTPError for bad responses in try block.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,7 @@ from requests.exceptions import HTTPError, ConnectionError
 if __name__ == "__main__":
     try:
         r = requests.get(sys.argv[1])
+        r.raise_for_status()
         print(r.text)
     except HTTPError as e:
         print(f"Error code: {e.response.status.code}")
