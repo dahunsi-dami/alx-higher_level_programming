697ed079410cd436a2815f0281da27e9fdb78788
dahunsi-dami <gbemigadare@gmail.com>
2024-04-30 18:34:13 +0100

    Send POST request to URL w/ requests, sys + other requirements.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Send POST request to http://0.0.0.0:5000/search_user
+w/ a letter stored in variable `q` as parameter
+w/ requests & sys only and show response body.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        val = ""
+    elif len(sys.argv) == 2:
+        val = sys.argv[1]
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': val})
+
+    try:
+        response = r.json()
+        if response:
+            for i in response:
+                user_id = i.get('id')
+                user_name = i.get('name')
+                print(f"[{user_id}] {user_name}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
