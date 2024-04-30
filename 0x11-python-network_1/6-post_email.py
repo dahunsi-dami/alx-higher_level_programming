8b86126c79d5f51ebe6ee0ee8da91964b5627323
dahunsi-dami <gbemigadare@gmail.com>
2024-04-30 17:34:28 +0100

    Pass parameter directly in requests line.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,5 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    payload = {"email": sys.argv[2]}
-    r = requests.post(sys.argv[1], params=payload)
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
