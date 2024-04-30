49edf28918b4ca3bb36efbeb3a9a7d3f321a7deb
dahunsi-dami <gbemigadare@gmail.com>
2024-04-30 17:30:39 +0100

    Send POST request to URL w/ email param, using requests & sys only.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Send POST request to URL passed w/ email as parameter
+w/ requests & sys only and show response body.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    payload = {"email": sys.argv[2]}
+    r = requests.get(sys.argv[1], params=payload)
+    print(r.text)
